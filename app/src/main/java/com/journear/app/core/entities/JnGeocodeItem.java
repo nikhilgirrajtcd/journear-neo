133c56f532694f93570576a57e320106d09f77bf
Nikhil Girraj <khandeln@tcd.ie>
2020-04-18 02:31:16 +0530

    Changed the geocoding to a single place name string instead of separated name, city, street and country.

--- app/src/main/java/com/journear/app/core/entities/JnGeocodeItem.java
+++ app/src/main/java/com/journear/app/core/entities/JnGeocodeItem.java
@@ -9,32 +9,12 @@ public class JnGeocodeItem {
     public String id;
     public double latitude = 0.0;
     public double longitude = 0.0;
-    public String name;
-    public String street;
-    public String city;
-    public String country;
+    // consolidated String for the name, as anything else is not needed in the whole workflow
+    public String placeString;
 
     @Override
     public String toString() {
-        String returnVal = "";
-        if (!StringUtils.isEmpty(name)) {
-            returnVal = name;
-
-            if (!StringUtils.isEmpty(street)) {
-                returnVal += ", " + street;
-            }
-
-            if (!StringUtils.isEmpty(city)) {
-                returnVal += ", " + city;
-            }
-
-            if (!StringUtils.isEmpty(country)) {
-                returnVal += ", " + country;
-            }
-        }
-        else
-            returnVal = StringUtils.joinWith(", ", name, street, city, country);
-        return returnVal;
+        return placeString;
     }
 
 
--- app/src/main/java/com/journear/app/core/utils/JnGeocoder.java
+++ app/src/main/java/com/journear/app/core/utils/JnGeocoder.java
@@ -14,81 +14,41 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class JnGeocoder {
-    public static ArrayList<JnGeocodeItem> GetGeocodingListForRegion(String region, Context context){
-        // check if the data is downloaded, use the function below
-
-        //return dummyData();
-        // return (ArrayList<JnGeocodeItem>) PersistentStore.getInstance().getItem("geocodes-" + region);
+    public static ArrayList<JnGeocodeItem> GetGeocodingListForRegion(String region, Context context) {
         return readGeocodingDataCsv(region, context);
 
     }
 
-    private static ArrayList<JnGeocodeItem> dummyData() {
-        ArrayList<JnGeocodeItem> dummyList = new ArrayList<>();
-        dummyList.add(new JnGeocodeItem() {{
-            city = "Dublin"; country = "Ireland";
-            latitude = 1;
-            longitude = 2;
-            id = "1";
-            name = "ABCD";
-            street = "Abbey Street";
-        }});
-        dummyList.add(new JnGeocodeItem() {{
-            city = "Dublin"; country = "Ireland";
-            latitude = 3;
-            longitude = 4;
-            id = "2";
-            name = "ACDC";
-            street = "Nassau Street";
-        }});
-        dummyList.add(new JnGeocodeItem() {{
-            city = "Dublin"; country = "Ireland";
-            latitude = 5;
-            longitude = 6;
-            id = "3";
-            name = "YOLO";
-            street = "Henry Street";
-        }});
-        return dummyList;
-    }
+    private static ArrayList<JnGeocodeItem> readGeocodingDataCsv(final String region, Context context) {
+        // At the moment the function only returns the data for region "ie", while the region field is not checked
 
-    private static ArrayList<JnGeocodeItem> readGeocodingDataCsv(final String region, Context context)
-    {
         ArrayList<JnGeocodeItem> returnSet = new ArrayList<>();
         String row = "";
 
-        try
-        {
-            InputStream csvStream = context.getResources().openRawResource(R.raw.geocodes_ie);
+        try {
+            InputStream csvStream = context.getResources().openRawResource(R.raw.geocodes_ie_fst);
             BufferedReader reader = new BufferedReader(new InputStreamReader(csvStream, Charset.forName("UTF-8")));
 
             reader.readLine(); // skip header
-            while( (row = reader.readLine()) != null)
-            {
-                final String[] cols = row.split(",", -1);
+            while ((row = reader.readLine()) != null) {
+                // Split the row, separate first 3 columns, and keep all the others together
+                final String[] cols = row.split(",", 4);
                 try {
                     JnGeocodeItem jnGeocodeItem = new JnGeocodeItem() {{
                         id = cols[0];
                         longitude = Double.parseDouble(cols[1]);
                         latitude = Double.parseDouble(cols[2]);
-                        name = cols[3];
-                        street = cols[4];
-                        city = cols[5];
-                        country = cols[6];
+                        placeString = cols[3];
                     }};
                     returnSet.add(jnGeocodeItem);
-                }
-                catch (NumberFormatException e)
-                {
+                } catch (NumberFormatException e) {
                     Log.e("JnGeocoder", "Lat/Longitude parsing error in row - " + row, e);
                 }
             }
 
             reader.close();
             csvStream.close();
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             Log.wtf("JnGeoCoder", "Error reading row from csv - " + row, ex);
         }
 
@@ -96,8 +56,7 @@ public class JnGeocoder {
     }
 
     // TODO: Nikhil
-    public static void DownloadAndStoreGeocodingDataForRegion(String region)
-    {
+    public static void DownloadAndStoreGeocodingDataForRegion(String region) {
         throw new UnsupportedOperationException("implementation pending");
     }
 }
