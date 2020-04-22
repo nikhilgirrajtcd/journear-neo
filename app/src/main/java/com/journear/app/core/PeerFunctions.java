c5a76fad288f73b020cbd9c816d6b843fdab6f68
Nikhil Girraj <khandeln@tcd.ie>
2020-04-23 01:19:00 +0530

    Added getBroadCastString method to use in Peer communication.

--- app/src/main/java/com/journear/app/core/PeerFunctions.java
+++ app/src/main/java/com/journear/app/core/PeerFunctions.java
@@ -2,6 +2,9 @@ package com.journear.app.core;
 
 import com.journear.app.core.entities.NearbyDevice;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Calendar;
 import java.util.LinkedList;
 
 public class PeerFunctions {
@@ -11,4 +14,15 @@ public class PeerFunctions {
         throw new UnsupportedOperationException("implementation pending");
     }
 
+    public static String getBroadcastString(NearbyDevice ndOwnJourneyPlan) {
+        if (ndOwnJourneyPlan == null) {
+            // TODO: Remove NULL Put the current user here, create a service locator
+            return getBroadcastString(NearbyDevice.getDummy());
+        }
+        return StringUtils.joinWith("|",
+                ndOwnJourneyPlan.getUser() == null ? "Dummy" : ndOwnJourneyPlan.getUser().userName,
+                ndOwnJourneyPlan.getSource2() == null ? -1 : ndOwnJourneyPlan.getSource2().id,
+                ndOwnJourneyPlan.getDestination2() == null ? -1 : ndOwnJourneyPlan.getDestination2().id,
+                ndOwnJourneyPlan.getTravelTime().toString());
+    }
 }
