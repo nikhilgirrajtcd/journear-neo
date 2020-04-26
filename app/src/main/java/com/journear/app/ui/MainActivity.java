46cfc4eadbfb78f3106840d9609ca9a7084accae
Sujit Jadhav <jadhav.sujit4@gmail.com>
2020-04-26 11:56:04 +0100

    rate the ride

--- app/src/main/java/com/journear/app/ui/MainActivity.java
+++ app/src/main/java/com/journear/app/ui/MainActivity.java
@@ -1,4 +1,4 @@
-    package com.journear.app.ui;
+package com.journear.app.ui;
 
 import android.content.ClipData;
 import android.content.ComponentName;
@@ -49,7 +49,7 @@ import java.util.Map;
 import java.util.Objects;
 
 
-    public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     private static final String LOGTAG = "MainActivityLogs";
     private AppBarConfiguration mAppBarConfiguration;
@@ -69,6 +69,10 @@ import java.util.Objects;
     protected void onCreate(Bundle savedInstanceState) {
         Intent intent = getIntent();
         ndOwnJourneyPlan = intent.getParcelableExtra("EXTRA");
+        if(intent.getBooleanExtra("fromRatingActivity", false)){
+            ndOwnJourneyPlan = null;
+            LocalFunctions.setCurrentJourney(ndOwnJourneyPlan);
+        }
 
         // Ask for permissions if user has revoked the permission manually after giving the permission for the first time
         LocalFunctions.requestPermissions(MainActivity.this);
@@ -393,9 +397,9 @@ import java.util.Objects;
 
         @Override
         public boolean equals(@Nullable Object obj) {
-            if(obj == null)
+            if (obj == null)
                 return false;
-            if(obj instanceof CachedComms) {
+            if (obj instanceof CachedComms) {
                 CachedComms com = (CachedComms) obj;
                 return Objects.equals(this.message.toReconstructableString(), com.message.toReconstructableString())
                         && Objects.equals(this.associatedRide, com.associatedRide);
@@ -426,7 +430,7 @@ import java.util.Objects;
         public void onResponse(JnMessage message, NearbyDevice associatedRide) {
             try {
                 CachedComms cachedComms = new CachedComms(message, associatedRide, false);
-                if(!cachedCommsList.contains(cachedComms)) {
+                if (!cachedCommsList.contains(cachedComms)) {
                     cachedCommsList.add(cachedComms);
                     pendingNotifications = getUnreadCachedCommsCount(cachedCommsList);
                     if (badgeCounter != null)
@@ -441,7 +445,7 @@ import java.util.Objects;
         public void onExpire(JnMessage expiredMessage, NearbyDevice nearbyDevice) {
             try {
                 CachedComms cachedComms = new CachedComms(expiredMessage, nearbyDevice, false);
-                if(!cachedCommsList.contains(cachedComms)) {
+                if (!cachedCommsList.contains(cachedComms)) {
                     cachedCommsList.add(cachedComms);
 
                     pendingNotifications = getUnreadCachedCommsCount(cachedCommsList);
---
+++ app/src/main/java/com/journear/app/ui/RateUserActivity.java
@@ -0,0 +1,49 @@
+package com.journear.app.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import com.journear.app.R;
+
+public class RateUserActivity extends AppCompatActivity {
+
+    RatingBar ratingbar;
+    Button rateButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_user);
+        addListenerOnButtonClick();
+    }
+
+    public void addListenerOnButtonClick() {
+        ratingbar = findViewById(R.id.ratingBar);
+        rateButton = findViewById(R.id.button_rate);
+        //Performing action on Button Click
+        rateButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                //Getting the rating and displaying it on the toast
+                String rating = String.valueOf(ratingbar.getRating());
+                final Intent intent = new Intent(RateUserActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                intent.putExtra("EXTRA", "");
+
+
+                startActivity(intent);
+                finish();
+
+                Toast.makeText(getApplicationContext(), rating, Toast.LENGTH_LONG).show();
+            }
+
+        });
+    }
+}
